6f64162307f254971f1845dba8afb07f61027d66
AlahiraJ <alahirajeffrey@gmail.com>
2021-11-08 20:23:40 +0100

    created models

---
+++ models/Cart.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const CartSchema = new mongoose.Schema({
+    userId : {type: String, required: true},
+    products : [
+        {
+            productId : {type : String},
+            quantity : {type : Number, default: 0 }
+        }
+    ]
+}, {timestamps: true})
+
+module.exports = mongoose.model('Cart', CartSchema);
