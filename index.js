6699dd2feec7a85aa07d1ba133da428285eab354
AlahiraJ <alahirajeffrey@gmail.com>
2021-11-17 23:03:45 +0100

    code refactor

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
-const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const productRoute = require("./routes/product");
 
 const port = process.env.PORT || 5000
 
 const app = express();
 
 //setup database
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL_LOCAL)
     .then(()=>{
         console.log("DB connection successful")
     }).catch((err)=>{
@@ -18,8 +21,11 @@ mongoose.connect(process.env.MONGO_URL)
 
 //configure express app
 app.use(express.json());
-app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/orders", orderRoute);
 
 //setup server
 app.listen(port, ()=>{
