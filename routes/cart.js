905b169bbf8e7cbe9dc25baba140d4e47d63e465
alahirajeffrey <alahirajeffrey@gmail.com>
2023-01-15 22:13:03 +0100

    code refactor

--- routes/cart.js
+++ routes/cart.js
@@ -1,75 +1,83 @@
-const router = require('express').Router();
-const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAmin} = require("./verifyToken");
+const router = require("express").Router();
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAmin,
+} = require("../utils/verifyToken");
 const Cart = require("../models/Cart");
 
 //create product
-router.post('/', verifyToken, async (req, res)=>{
+router.post("/", verifyToken, async (req, res) => {
+  //validate request
+  if (!req.body.userId)
+    return res.status(400).send({ message: "Ooops. UserId is required.." });
 
-    //validate request
-    if(!req.body.userId) return res.status(400).send({message : "Ooops. UserId is required.."})
-    
-    const newCart = new Cart(req.body);
+  const newCart = new Cart(req.body);
 
-    try{
-        const savedCart = await newCart.save();
-        res.status(200).json(savedCart);
-    }catch(err){
-        res.status(500).json(err);
-    }
-})
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //update product
-router.put('/:id', verifyTokenAndAuthorization, async (req, res)=>{
-    
-    //validate request
-    if(!req.params.id) return res.status(400).send({message : "Ooops. Id is required.."})
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  //validate request
+  if (!req.params.id)
+    return res.status(400).send({ message: "Ooops. Id is required.." });
 
-    try{
-        const updatedCart = await Cart.findByIdAndUpdate(req.params.id, {
-            $set: req.body},
-            {new : true})
-        res.status(200).json(updatedCart)
-    }catch(err){
-        res.status(500).json(err);
-    }
-})
+  try {
+    const updatedCart = await Cart.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //delete product
-router.delete("/:id", verifyTokenAndAuthorization, async (req, res)=>{
-    
-    //validate request
-    if(!req.params.id) return res.status(400).send({message : "Ooops. Id is required.."})
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  //validate request
+  if (!req.params.id)
+    return res.status(400).send({ message: "Ooops. Id is required.." });
 
-    try{
-        await Cart.findByIdAndDelete(req.params.id)
-        res.status(200).json("Cart has been deleted")
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+  try {
+    await Cart.findByIdAndDelete(req.params.id);
+    res.status(200).json("Cart has been deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //get user product
-router.get('/findOne/:id', verifyTokenAndAuthorization, async (req, res)=>{
-    
-    //validate request
-    if(!req.params.id) return res.status(400).send({message : "Ooops. UserId is required.."})
+router.get("/findOne/:id", verifyTokenAndAuthorization, async (req, res) => {
+  //validate request
+  if (!req.params.id)
+    return res.status(400).send({ message: "Ooops. UserId is required.." });
 
-    try{
-        const cart = await Cart.findOne({userId : req.params.id})
-        res.status(200).json(cart)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+  try {
+    const cart = await Cart.findOne({ userId: req.params.id });
+    res.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //get all carts
-router.get('/' , verifyTokenAndAmin, async (req, res)=>{
-    try{
-        const carts = await Cart.find()
-        req.status(200).json(carts)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get("/", verifyTokenAndAmin, async (req, res) => {
+  try {
+    const carts = await Cart.find();
+    req.status(200).json(carts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,52 +1,61 @@
-const router = require('express').Router();
-const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAmin} = require("../utils/verifyToken");
-const CryptoJS = require('crypto-js');
+const router = require("express").Router();
+const { verifyTokenAndAuthorization } = require("../utils/verifyToken");
+const CryptoJS = require("crypto-js");
 const User = require("../models/User");
 
 //update user
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  //validate request
+  if (!req.body.username)
+    return res.status(400).send({ message: "Ooops. Username missing.." });
+  if (!req.body.email)
+    return res.status(400).send({ message: "Ooops. Email missing.." });
+  if (!req.body.password)
+    return res.status(400).send({ message: "Ooops. Password missing.." });
 
-    //validate request
-    if(!req.body.username) return res.status(400).send({message : "Ooops. Username missing.."})
-    if(!req.body.email) return res.status(400).send({message : "Ooops. Email missing.."})
-    if(!req.body.password) return res.status(400).send({message : "Ooops. Password missing.."})
+  //check password
+  if (req.body.password) {
+    //encrypt password
+    hashedPassword = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SEC_PASS
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
 
-     //check password
-     if(req.body.password){
-        //encrypt password
-        hashedPassword = CryptoJS.AES.encrypt(req.body.password, process.env.SEC_PASS).toString();
-    }
-    try{
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-            $set : req.body}, 
-            {new: true});
-            
-        res.status(200).json({
-            "userId" : updatedUser.id,
-            "username":updatedUser.username,
-            "email" : updatedUser.email,
-            "isAdmin" : updatedUser.isAdmin,
-            "dateCreated" : updatedUser.createdAt,
-            "accessToken" : updatedUser.accessToken
-        });
-    }catch(err){
-        return res.status(500).json(err);
-    }
-})
+    res.status(200).json({
+      userId: updatedUser.id,
+      username: updatedUser.username,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      dateCreated: updatedUser.createdAt,
+      accessToken: updatedUser.accessToken,
+    });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 //delete user
-router.delete("/:id", verifyTokenAndAuthorization, async (req, res)=>{
-    
-    //validate request
-    if(!req.params.id) return res.status(400).send({message : "Ooops. User id required.."})
-    
-    try{
-        await User.findByIdAndDelete(req.params.id)
-        res.status(200).json("User has been deleted")
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  //validate request
+  if (!req.params.id)
+    return res.status(400).send({ message: "Ooops. User id required.." });
+
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //get single user
 // router.get("/:id", verifyTokenAndAmin, async (req, res)=>{
